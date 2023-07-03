a313adb1925b71789354d75f400e91ab98696f9e
Cicada Carpenter <cicada8802@gmail.com>
2023-07-03 17:59:43 -0600

    Create SW Trivia Quiz Lists.py

---
+++ SW Trivia Quiz Lists.py
@@ -0,0 +1,12 @@
+questions.append("On average how much of earth is covered by the ocean?")
+    answers.append(["About 70%", "About 50%",
+        "About 80%", "About 10%"])
+    correct_answers.append("a")
+questions.append("On average how much of earth is covered by the Pacific Ocean?")
+    answers.append(["About 10%", "About 50%",
+        "About 30%", "About 90%"])
+    correct_answers.append("c")
+questions.append("On average how deep is Mariana Trench?")
+    answers.append(["About 20km", "About 11km",
+        "About 30km", "About 6km"])
+    correct_answers.append("b")
