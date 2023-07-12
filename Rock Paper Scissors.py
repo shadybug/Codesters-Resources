6976f85cbfc78564265568033f225a11ead63c0e
Cicada Carpenter <cicada8802@gmail.com>
2023-07-12 16:02:41 -0600

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,10 +1,13 @@
-# Initialization here
+# INITIALIZATION HERE
 
+# Main game loop (like a forever loop in Scratch)
 while True:
-    # Insert code to pick player/computer choices here
-    
+    # INSERT CODE TO PICK PLAYER/COMPUTER CHOICES HERE
+
+    # Make a text label
     winner = codesters.Text("Hello up here.", 0, 200, "white")
-    
+
+    # Check who won with a series of conditionals, and change the text of the label
     if choice == computer_choice:
         winner.set_text("Tie!")
     elif choice == "rock":
@@ -22,9 +25,11 @@ while True:
             winner.set_text("Computer wins!")
         else:
             winner.set_text("You win!")
-    
+
+    # Wait a few seconds before resetting
     stage.wait(3)
     player.load_image("astronaut1") # Replace with your sprite names
     computer.load_image("alien1") # Replate with your sprite names
-    
+
+    # Delete the text label so it no longer shows
     stage.remove_sprite(winner)
