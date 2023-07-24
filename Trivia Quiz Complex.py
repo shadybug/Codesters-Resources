c2de82160c0ee709a263fb366a9bb3e8a54c593e
Cicada Carpenter <cicada8802@gmail.com>
2023-07-24 17:53:07 -0600

    Update Trivia Quiz Complex.py

--- Trivia Quiz Complex.py
+++ Trivia Quiz Complex.py
@@ -1,41 +1,11 @@
-# Put this code directly after setting the sprite and stage
-# Text labels
-question = codesters.Text("test question", 0, 180, "white")
-a = codesters.Text("A:", -150, 50, "white")
-b = codesters.Text("B:", 150, 50, "white")
-c = codesters.Text("C:", -150, -50, "white")
-d = codesters.Text("D:", 150, -50, "white")
-
-choices = [a,b,c,d]
-
-# choices on the screen
-questions = []
-answers = []
-correct_answers = []
-
-========SEPARATION OF SECTIONS: DO NOT COPY=========
-
-# Replace your for loop, and all other code after it, with this. Don't delete the create_questions() function!
-for counter in range (3):
-    question.set_text(questions[counter])
-    
-    a.set_text("A: " + answers[counter][0])
-    b.set_text("B: " + answers[counter][1])
-    c.set_text("C: " + answers[counter][2])
-    d.set_text("D: " + answers[counter][3])
-    
-    choice = sprite.ask(questions[counter])
-    
-    # Check if the question was correct, and increase the score
-    if choice == correct_answers[counter]:
-        sprite.say("Correct!")
+# Click event
+def answer_click(sprite):
+    if sprite is correct_answers[0]:
+        question.set_text("Correct!")
         score = score + 1
-    # Otherwise, tell the player they got it wrong
-    else:
-        sprite.say("Incorrect! The correct answer was C.")
-    
-    # Wait a bit, so the player can see the message
-    stage.wait(2)
 
-# Tell the player their score
-sprite.say(score)
+# Register the click event to all text
+a.event_click(answer_click)
+b.event_click(answer_click)
+c.event_click(answer_click)
+d.event_click(answer_click)
