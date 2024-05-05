4ed3d1c31a9e698a384496e5ab19df160e25202c
Cicada Carpenter <cicada8802@gmail.com>
2024-05-05 12:15:10 -0600

    Update Space Age.py

--- Space Age.py
+++ Space Age.py
@@ -1,46 +1,42 @@
-#set background
-stage.set_background("scrollingspace")
-
-#set up the sprites
-p1_sprite = "https://i.ibb.co/4N3tSDH/Bevouliin-dino-spaceship-flying-game-character.png"
-p1 = codesters.Sprite(p1_sprite, -220, 0)
-p1.set_size(0.1)
-p1.lives = 3
-p2_sprite = "https://i.ibb.co/PTsxxr3/Bevouliin-smiling-spaceship-game-character.png"
-p2 = codesters.Sprite(p2_sprite, 220, 0)
-p2.set_size(0.1)
 p2.flip_right_left()
-p2.lives = 3
 
+p1.set_size(0.25)
+p2.set_size(0.25)
 
-#controls for both ships
+# stage setup
+stage.disable_all_walls()
+
+# player movements
 def w_key():
-    p1.move_up(10)
-stage.event_key("w", w_key)
-def s_key():
-    p1.move_down(10)
-stage.event_key("s", s_key)
+    p1.move_up(20)
 
+def s_key():
+    p1.move_down(20)
 
 def up_key():
-    p2.move_up(10)
-stage.event_key("up", up_key)
+    p2.move_up(20)
+
 def down_key():
-    p2.move_down(10)
+    p2.move_down(20)
+    
+stage.event_key("w", w_key)
+stage.event_key("s", s_key)
+stage.event_key("up", up_key)
 stage.event_key("down", down_key)
 
-left_firing = False
-right_firing = False
+# fire lasers
+def d_key():
+    fire(p1, 5, "red")
 
-#fires a laser for player 1
-def left_fire():
-    global left_firing
-    if left_firing is not True:
-        left_firing = True
-        laser = codesters.TriangleIso(p1.get_x(), p1.get_y(), 5, 10, "blue")
-        laser.turn_right(90)
-        laser.set_x_speed(10)
-        left_firing = False
+def left_key():
+    fire(p2, -5, "orange")
+    
+stage.event_key("d", d_key)
+stage.event_key("left", left_key)
 
-
-
+def fire(player, direction, color):
+    # Make a laser sprite, start at the player that fired it
+    laser = codesters.Rectangle(player.get_x(), player.get_y(), 10, 5, color)
+    # Move across the screen
+    laser.set_x_speed(direction)
+    stage.wait(0.25)
