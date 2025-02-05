4d73b5f4afbf10364e214f4e2993bd699cef8dcc
Cicada Carpenter <cicada8802@gmail.com>
2025-02-05 14:58:34 -0700

    Update Wizard Quest.py

--- Wizard Quest.py
+++ Wizard Quest.py
@@ -1,56 +1,41 @@
-# Stage setup
-stage.set_background_color("green")
-stage.disable_all_walls()
+# Stage and player setup
+stage.set_background_color("black")
 
-# Player setup (dragon)
-player = codesters.Sprite("dragon")
-player.set_size(0.4)
+player = codesters.Sprite("ufo3")
+player.set_size(0.15)
 
-# Enemy setup (wizard)
-enemy = codesters.Sprite("evilwizard", -100, 100)
-enemy.set_size(0.25)
-
-# List to keep track of all non-player sprites
+# List of all sprites
 all_sprites = []
 
-# Create trees at random locations and add them to the list
-def create_scenery():
-    for counter in range(5):
-        tree_x = random.randint(-250, 250)
-        tree_y = random.randint(-250, 250)
-        tree = codesters.Sprite("pinetree2", tree_x, tree_y)
-        tree.set_size(0.35)
-        all_sprites.append(tree)
-create_scenery()
+# Scenery sprite generation
+for counter in range(5):
+    x = random.randint(-250, 250)
+    y = random.randint(-250, 250)
 
-# Movement - this moves all sprites around the player,
-# the player stays in the center of the screen
+    asteroid = codesters.Sprite("asteroid3", x, y)
+    asteroid.set_size(0.25)
+    all_sprites.append(asteroid)
+
+# Player movement
 def up_key():
-    enemy.move_down(20)
     for sprite in all_sprites:
-        sprite.move_down(20)
+        sprite.move_up(20)
 stage.event_key("w", up_key)
 
 def down_key():
-    enemy.move_up(20)
     for sprite in all_sprites:
-        sprite.move_up(20)
+        sprite.move_down(20)
 stage.event_key("s", down_key)
 
 def left_key():
-    enemy.move_right(20)
     for sprite in all_sprites:
-        sprite.move_right(20)
+        sprite.move_left(20)
 stage.event_key("a", left_key)
 
 def right_key():
-    enemy.move_left(20)
     for sprite in all_sprites:
-        sprite.move_left(20)
+        sprite.move_right(20)
 stage.event_key("d", right_key)
 
-# Interval event that causes the enemy to rotate every 2 seconds
-# (setup for attacking/moving code)
-def enemy_fire():
-    enemy.turn_left(15)
-stage.event_interval(enemy_fire, 2)
+
+
